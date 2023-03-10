33ddf4d9bd4d812f5494b85312eec4dcaffe03c9
Matheus Lopes <mlopes.matheus@gmail.com>
2023-03-10 15:30:33 -0300

    init home page

--- lib/home/view/datail_page.dart
+++ lib/home/view/datail_page.dart
@@ -1,5 +1,9 @@
+import 'dart:async';
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:how_much/home/view/home_page.dart';
+import 'package:http/http.dart' as http;
 
 class DetailScreen extends StatelessWidget {
   const DetailScreen({required this.salaryInfos, super.key});
@@ -12,10 +16,44 @@ class DetailScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text('so..'),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Text(salaryInfos.amountValue.toString()),
+      body: Container(
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(salaryInfos.amountValue.toString()),
+            Center(
+              child: FutureBuilder<CountryDetail>(
+                future: fetchCountryDetail(
+                    http.Client(), salaryInfos.amountValue ?? 0),
+                builder: (context, snapshot) {
+                  if (snapshot.hasError) {
+                    return const Center(
+                      child: Text('An error has occurred!'),
+                    );
+                  } else if (snapshot.hasData) {
+                    return Text(snapshot.data!.result);
+                  } else {
+                    return const Center(
+                      child: CircularProgressIndicator(),
+                    );
+                  }
+                },
+              ),
+            ),
+          ],
+        ),
       ),
     );
+  }
+
+  Future<CountryDetail> fetchCountryDetail(
+      http.Client http, amountValue) async {
+    final url = 'http://172.17.0.1/brazil/$amountValue';
+    final response = await http.get(Uri.parse(url));
+
+    final jsonResponse = jsonDecode(response.body) as Map<String, dynamic>;
+
+    return CountryDetail.fromJson(jsonResponse);
   }
 }
--- lib/home/view/home_page.dart
+++ lib/home/view/home_page.dart
@@ -4,9 +4,27 @@ import 'package:flutter/services.dart';
 import 'package:how_much/home/view/datail_page.dart';
 
 class SalaryInfos {
-  const SalaryInfos({required this.amountValue});
+  SalaryInfos(double amountValue, List<CountryDetail> countryDetail) {
+    this.amountValue = amountValue;
+    this.countryDetail = countryDetail;
+  }
+  //SalaryInfos(this.countryDetail, this.amountValue);
+  double? amountValue;
+  List<CountryDetail>? countryDetail;
+}
 
-  final double amountValue;
+class CountryDetail {
+  final String country;
+  final String result;
+
+  const CountryDetail({required this.country, required this.result});
+
+  factory CountryDetail.fromJson(Map<String, dynamic> json) {
+    return CountryDetail(
+      country: json['country'] as String,
+      result: json['result'] as String,
+    );
+  }
 }
 
 class MainForm extends StatefulWidget {
@@ -27,7 +45,7 @@ class _MainFormState extends State<MainForm> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('good morning, beautiful proletariat!')),
+      appBar: AppBar(title: const Text('Good morning, beautiful proletariat!')),
       body: Container(
         padding: const EdgeInsets.all(8),
         child: Form(
@@ -45,21 +63,22 @@ class _MainFormState extends State<MainForm> {
                 padding: const EdgeInsets.symmetric(vertical: 16),
                 child: ElevatedButton(
                   onPressed: () {
-                    if (_formKey.currentState!.validate()) {
+                    if (_formKey.currentState?.validate() ?? false) {
                       double value =
                           _formatter.getUnformattedValue().toDouble();
-
+                      //TODO: Try named route approach (https://docs.flutter.dev/cookbook/navigation/navigate-with-arguments)
                       Navigator.push(
                         context,
                         MaterialPageRoute(
                           builder: (context) => DetailScreen(
-                            salaryInfos: SalaryInfos(amountValue: value),
+                            // salaryInfos: SalaryInfos(amountValue: value),
+                            salaryInfos: SalaryInfos(value, List.empty()),
                           ),
                         ),
                       );
                     }
                   },
-                  child: const Text('Submit'),
+                  child: const Text('Do the magic'),
                 ),
               ),
             ],
